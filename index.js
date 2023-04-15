d108dcf4285aaa6d8954044291017b30e3e634ec
JoaoHanzelDemetrio <102702861+JoaoHanzelDemetrio@users.noreply.github.com>
2023-04-15 00:28:20 +0000

    resolvido2

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ function background() {
    *
    * @param {*} node Um objeto HTML Node
    */
+  
   function removeAllChildren(elementoId) {
       const elemento = document.getElementById(elementoId);
       while (elemento.firstChild) {
