edee3b7200084001b1d9fa4e9c80b3b8a2ec6fe2
JoaoHanzelDemetrio <102702861+JoaoHanzelDemetrio@users.noreply.github.com>
2023-04-15 00:20:37 +0000

    finalizado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Aplica a cor #BECCC3 no background do body da página
  */
-function background(){
+function background() {
 
     /**
      * Tarefa/Issue  1
@@ -9,58 +9,72 @@ function background(){
      * Desenvolva uma função capaz de trocar a cor do fundo da página, ou seja,
      * alterar o background do body para a cor #BECCC3.
      */
-
-}
-
-/**
- * Recupera os valores dos campos de texto cujo o id='fname' e id='lname'
- * e apresente o nome e o sobrenome de uma pessoa (separado por um espaço)
- * na div id='result'
- */
-function show(){
-
-     /**
-      * Issue  2
-      *
-      * Recupera os valores dos campos de texto cujo o id='fname' e id='lname' e apresente o nome
-      * e o sobrenome de uma pessoa (separado por um espaço) na div id='result'
-      */
-}
-
-/**
- * Filtra o array de objetos (data) de acordo com os caracteres
- * digitados no campo de texto id='name'
- */
-function search(){
-
-    var data = [{name: "Rodrigo"}, {name: "Ricardo"}, {name: "Fabio"}, {name: "Alex"}, {name: "Sílvia"}];
-
+    ;
+  
+    document.body.style.backgroundColor = "#BECCC3"
+  
+  }
+  
+  /**
+   * Recupera os valores dos campos de texto cujo o id='fname' e id='lname'
+   * e apresente o nome e o sobrenome de uma pessoa (separado por um espaço)
+   * na div id='result'
+   */
+  function show() {
+  
     /**
-     * Issue 3
+     * Issue  2
      *
-     * Utilizando o array (data) de objetos JSON acima, desenvolva um código que permita com que os
-     * usuários procurem pelo nome de pessoas. Assim, quando o usuário digitar a letra 'r' no campo
-     * de texto (id='name'), todas as pessoas que iniciarem com essa letra devem ser apresentados.
-     * Consequentemente, quando for digitado 'ro' o sistema deve filtrar e apresentar na div com o
-     * id='search' as pessoas cujo o nome inicia com 'ro'.
-     *
-     * Segue algumas funções que podem auxiliar na resolução da questão:
-     *      substr - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substr
-     *      length - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/length
-     *      toUpperCase - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
-     *
-     * Além disso, a função removeAllChildren abaixo também pode ser útil para o desenvolvimento da solução
+     * Recupera os valores dos campos de texto cujo o id='fname' e id='lname' e apresente o nome
+     * e o sobrenome de uma pessoa (separado por um espaço) na div id='result'
      */
-
-}
-
-/**
- * Remove todos os filhos de um nó
- *
- * @param {*} node Um objeto HTML Node
- */
-function removeAllChildren(node){
-    while (node.hasChildNodes()) {
-        node.removeChild(node.firstChild);
+  
+    const nome = document.getElementById("fname").value;
+    const sobrenome = document.getElementById("lname").value;
+    const nomeSobrenome = `${nome} ${sobrenome}`;
+    document.getElementById("result").innerHTML = nomeSobrenome;
+  
+  
+  }
+  
+  /**
+   * Filtra o array de objetos (data) de acordo com os caracteres
+   * digitados no campo de texto id='name'
+   */
+  function search() {
+  
+    var data = [{ name: "Rodrigo" }, { name: "Ricardo" }, { name: "Fabio" }, { name: "Alex" }, { name: "Sílvia" }];
+  
+    const input = document.getElementById("name").value.toUpperCase();
+    const pesquisa = [];
+  
+    for (let i = 0; i < data.length; i++) {
+      const name = data[i].name.toUpperCase();
+      if (name.indexOf(input) === 0) {
+        pesquisa.push(data[i].name);
+      }
     }
-}
+    removeAllChildren("search");
+  
+    for (let i = 0; i < pesquisa.length; i++) {
+      const div = document.createElement("div");
+      const text = document.createTextNode(pesquisa[i]);
+      div.appendChild(text);
+      document.getElementById("search").appendChild(div);
+    }
+  
+  
+  }
+  
+  /**
+   * Remove todos os filhos de um nó
+   *
+   * @param {*} node Um objeto HTML Node
+   */
+  function removeAllChildren(elementoId) {
+      const elemento = document.getElementById(elementoId);
+      while (elemento.firstChild) {
+        elemento.removeChild(elemento.firstChild);
+      }
+  }
+  
